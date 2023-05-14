f8d6935808b45f0238f180100f1da7099a0d9ceb
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-05-15 01:14:05 +0900

    feat: building CV and reja application

--- app.js
+++ app.js
@@ -1,86 +1,3 @@
-// 2: Session code
-
-// // 3 Views code
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-
-// // 4 Routing code
-// app.post("/create-item", (req, res) => {
-//   //TODO: code with db here
-// });
-
-// app.get("/", function (req, res) {
-//   res.render("harid");
-// });
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log(`The server is running successfully on port: ${PORT}`);
-//});
-
-// 10 ASYNCHRONOUS FUNCTIONLARNI QOLLASH/////
-
-// console.log("Web Serverni boshlash");
-// const express = require("express");
-// const res = require("express/lib/response");
-// const app = express();
-// const http = require("http");
-// const fs = require("fs");
-
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-
-// // 1 Kirish code
-// app.use(express.static("public"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // 2: Session code
-
-// // 3 Views code
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-
-// // 4 Routing code
-// app.post("/create-item", (req, res) => {
-//   //TODO: code with db here
-// });
-
-// app.get("/", function (req, res) {
-//   res.render("harid");
-// });
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log(`The server is running successfully on port: ${PORT}`);
-// });
-
-//11 mongo_atlas
-
-// console.log("Web Serverni boshlash");
-// const express = require("express");
-// const res = require("express/lib/response");
-// const app = express();
-// const http = require("http");
-// const fs = require("fs");
-
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-
 //  MongoDB connect
 
 console.log("Web Serverni boshlash");
@@ -90,7 +7,15 @@ const app = express();
 
 // MongoDB choqirish
 const db = require("./server").db();
-
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -105,6 +30,10 @@ app.set("view engine", "ejs");
 // 4 Routing code
 app.post("/create-item", (req, res) => {
   //TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
