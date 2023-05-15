662b31ba4c1f5f15b4af39ea9aec19ad7c57b4b3
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-05-16 00:32:27 +0900

    feat: GRUD operatsiyalari

--- app.js
+++ app.js
@@ -29,15 +29,37 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
+  console.log("user entered / create-item");
+  console.log(req.body);
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
   //TODO: code with db here
 });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+// app.get("/author", (req, res) => {
+//   res.render("author", { user: user });
+// });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        console.log(data);
+        res.render("reja", { item: data });
+      }
+    });
 });
 
 module.exports = app;
