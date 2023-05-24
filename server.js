6cc77a3bd571df23066231c3e540701793324c34
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-05-24 11:39:20 +0900

    feat: server.js host ip change 3011

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 3011;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT} `
